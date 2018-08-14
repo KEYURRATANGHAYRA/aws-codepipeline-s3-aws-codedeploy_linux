30e932b87f9f5d4a72d3a7f53cf30f4f7f9da5bd
Keyur's Kitchen <keyurratanghayra@gmail.com>
2018-08-14 12:28:06 +0530

    Create validate_curl.sh

---
+++ scripts/validate_curl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export NO_PROXY=localhost,169.254.169.254
+result=$(/usr/bin/curl -o -I -L  -w "%{http_code}" http://`hostname`:80)
+
+if [[ "$result" =~ "200" ]]; then
+    echo "OneSearch deployment succeeded"
+    exit 0
+else
+    echo "OneSearch deployment failed"
+        exit 1
+fi
