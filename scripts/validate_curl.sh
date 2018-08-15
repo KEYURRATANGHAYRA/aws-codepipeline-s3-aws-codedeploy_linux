6401c8bb20a9327a235304c267fa0406dfda32f3
Keyur's Kitchen <keyurratanghayra@gmail.com>
2018-08-15 11:43:20 +0530

    Update validate_curl.sh

--- scripts/validate_curl.sh
+++ scripts/validate_curl.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+netstat -plant | grep 80
+curl http://`hostname`:80
 export NO_PROXY=localhost,169.254.169.254
 result=$(/usr/bin/curl -o -I -L  -w "%{http_code}" http://`hostname`:80)
 
